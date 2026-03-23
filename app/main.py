be4565848405ed39043b01b9e9c103d89c9e0847
GJLG <A01660262@tec.mx>
2026-03-23 13:29:26 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.router import api_router
 
-app = FastAPI(title="Feria Servicio Social API", version="0.1.0")
+app = FastAPI(title="Feria de Servicio Social Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router, prefix="/api/v1")
